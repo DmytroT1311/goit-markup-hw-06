de97c534707da8ec8897becaa7143e0427e754cb
DmytroT1311 <dmytro.tiutiunnyk1@gmail.com>
2024-01-28 17:59:46 +0200

    9

---
+++ js/app.js
@@ -0,0 +1,35 @@
+(() => {
+    const refs = {
+        openModalBtn: document.querySelectorAll("[data-modal-open]"),
+        closeModalBtn: document.querySelector("[data-modal-close]"),
+        modal: document.querySelector("[data-modal]"),
+    };
+
+    refs.openModalBtn.forEach((element) => {
+        element.addEventListener("click", toggleModal);
+    });
+    refs.closeModalBtn.addEventListener("click", toggleModal);
+
+    //* Закривання по кліку на бекдроп
+
+    refs.modal.addEventListener("click", removeModal);
+    function removeModal(e) {
+        if (e.target === e.currentTarget) {
+            refs.modal.classList.add("is-hidden");
+        }
+    }
+
+    function toggleModal() {
+        refs.modal.classList.toggle("is-hidden");
+        document.body.classList.toggle("no-scroll");
+	}
+	
+	document.querySelector(".hero__btn").addEventListener("click", toggleOrderServiceModal);
+	document.querySelector(".modal__close-btn").addEventListener("click", toggleOrderServiceModal);
+;
+	function toggleOrderServiceModal() {
+        document.querySelector(".backdrop").classList.toggle("is-open");
+        document.body.classList.toggle("no-scroll");
+	}
+	
+})();
